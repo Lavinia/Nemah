2bda4b40c6e0a6258e9ddadf6fdc0efa751e1848
Lennart and Kim <lennart@devl.se>
2013-10-12 12:25:55 +0200

    Add feedability

--- lib/nemah.rb
+++ lib/nemah.rb
@@ -8,12 +8,26 @@ module Nemah
       end
     end
 
-    attr_reader :weight, :gender, :name
+    attr_reader :weight, :gender, :name, :feedability
 
     def initialize(attributes)
       @weight = attributes.fetch(:weight) { raise AttributeRequiredError, :weight }
       @gender = attributes.fetch(:gender) { raise AttributeRequiredError, :gender }
       @name = attributes.fetch(:name, '')
+      @feedability = attributes.fetch(:feedability, :normal)
+      assert_valid_feedability
+    end
+
+    private
+
+    def assert_valid_feedability
+      unless allowed_feedabilities.include? feedability
+        raise ArgumentError, "\"#{feedability.inspect}\" is not an allowed feedability"
+      end
+    end
+
+    def allowed_feedabilities
+      [:easy, :normal, :hard]
     end
   end
 end
--- spec/nemah_spec.rb
+++ spec/nemah_spec.rb
@@ -14,14 +14,40 @@ describe Nemah do
       expect(a_horse_named('Nemah').name).to eq('Nemah')
     end
 
+    it 'is considered to be of normal difficulty to feed' do
+      expect(a_horse.feedability).to eq(:normal)
+    end
+
+    it 'can be hard to feed' do
+      expect(a_horse_with(feedability: :hard).feedability).to eq(:hard)
+    end
+
+    it 'can be easy to feed' do
+      expect(a_horse_with(feedability: :easy).feedability).to eq(:easy)
+    end
+
+    it 'cannot be impossible to feed' do
+      expect {
+        a_horse_with(feedability: :impossible)
+      }.to raise_error(ArgumentError, '":impossible" is not an allowed feedability')
+    end
+
     private
 
-    def a_horse_named(name)
-      Nemah::Horse.new(default_attributes.merge(name: name))
+    def a_horse
+      Nemah::Horse.new(default_attributes)
+    end
+
+    def a_horse_with(attributes)
+      Nemah::Horse.new(default_attributes.merge(attributes))
     end
 
     def a_horse_without(attribute)
       Nemah::Horse.new(default_attributes.delete_if { |key, _| key == attribute })
+    end
+
+    def a_horse_named(name)
+      a_horse_with(name: name)
     end
 
     def default_attributes
