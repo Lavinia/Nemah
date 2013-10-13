01f11692294923798785d2dac0524911f8bd4dde
Lennart and Kim <lennart@devl.se>
2013-10-13 18:50:09 +0200

    Simplify specs

--- spec/nemah/need_spec.rb
+++ spec/nemah/need_spec.rb
@@ -3,23 +3,31 @@ require 'spec_helper'
 describe Nemah::Need do
   describe 'calculating solids' do
     it 'for an arabian princess' do
-      horse = Nemah::Horse.new(weight: 450, gender: :mare)
-      need = Nemah::Need.new(horse)
-      expect(need.solids).to eq(6.75..Float::INFINITY)
+      expect(need_for_nemah.solids).to eq(6.75..Float::INFINITY)
     end
 
     it 'for a huge horse' do
-      horse = Nemah::Horse.new(weight: 1200, gender: :stallion)
-      need = Nemah::Need.new(horse)
-      expect(need.solids).to eq(18.00..Float::INFINITY)
+      expect(need_for_samson.solids).to eq(18.00..Float::INFINITY)
     end
   end
 
   describe 'calculating selenium' do
     it 'for an arabian princess' do
-      horse = Nemah::Horse.new(weight: 450, gender: :mare)
-      need = Nemah::Need.new(horse)
-      expect(need.selenium).to eq(0.9..22.5)
+      expect(need_for_nemah.selenium).to eq(0.9..22.5)
     end
+  end
+
+  private
+
+  def need_for_horse_with(args)
+    Nemah::Need.new(Nemah::Horse.new(args))
+  end
+
+  def need_for_samson
+    need_for_horse_with(weight: 1200, gender: :stallion)
+  end
+
+  def need_for_nemah
+    need_for_horse_with(weight: 450, gender: :mare)
   end
 end
