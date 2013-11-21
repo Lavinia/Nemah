c8eb0baaf9e3b7bfc69bddedb92c4f4685c8cc0c
Lennart and Kim <lennart@devl.se>
2013-11-21 21:22:13 +0100

    Add calcium need for non-working horse.

--- lib/nemah/specific_need/calcium.rb
+++ lib/nemah/specific_need/calcium.rb
@@ -1,6 +1,23 @@
 module Nemah
   module SpecificNeed
     class Calcium < AbstractNeed
+      def max(decimals: 2)
+        _max
+      end
+
+      private
+
+      def _ideal
+        _min
+      end
+
+      def _min
+        horse.weight_in_deciton * 4.0
+      end
+
+      def _max
+        Float::INFINITY
+      end
 
     end
   end
--- spec/nemah/specific_need/calcium_spec.rb
+++ spec/nemah/specific_need/calcium_spec.rb
@@ -2,4 +2,52 @@ require 'spec_helper'
 
 describe Nemah::SpecificNeed::Calcium do
   it_behaves_like 'a specific need'
+
+  describe '#to_rounded_range' do
+    it 'returns a range between minimum and maximum allowed amounts, rounded to two decimals' do
+      expect(calcium.to_rounded_range).to eq(21.20..Float::INFINITY)
+    end
+
+    it 'optionally takes the number of decimals' do
+      expect(calcium.to_rounded_range(decimals: 0)).to eq(21..Float::INFINITY)
+    end
+  end
+
+  describe '#ideal' do
+    it 'returns the ideal amount of calcium needed' do
+      expect(calcium.ideal).to eq(21.20)
+    end
+
+    it 'optionally takes the number of decimals' do
+      expect(calcium.ideal(decimals: 0)).to eq(21)
+    end
+  end
+
+  describe '#min' do
+    it 'returns the minimum calcium need' do
+      expect(calcium.min).to eq(21.20)
+    end
+
+    it 'optionally takes the number of decimals' do
+      expect(calcium.min(decimals: 0)).to eq(21)
+    end
+  end
+
+  describe '#max' do
+    it 'returns the maximum calcium need' do
+      expect(calcium.max).to eq(Float::INFINITY)
+    end
+
+    it 'optionally takes the number of decimals' do
+      expect(calcium.max(decimals: 0)).to eq(Float::INFINITY)
+    end
+  end
+
+  def calcium
+    Nemah::SpecificNeed::Calcium.new(need)
+  end
+
+  def need
+    double(:horse => double(:weight_in_deciton => 5.3))
+  end
 end
