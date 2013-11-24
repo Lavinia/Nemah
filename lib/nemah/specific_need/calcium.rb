a181a3aa175443fcbc233695874d53a9278b1690
Lennart and Kim <lennart@devl.se>
2013-11-24 19:59:50 +0100

    Extract FactorTable class.

--- lib/nemah/specific_need/calcium.rb
+++ lib/nemah/specific_need/calcium.rb
@@ -20,36 +20,53 @@ module Nemah
       end
 
       def energy_and_workload_factor
-        EnergyWorkloadRatio.new(energy.for_workload, energy.for_maintenance).factor
+        EnergyWorkloadRatio.new(energy.for_workload, energy.for_maintenance, factor_table).factor
       end
 
       def energy
         need.energy
       end
 
+      def factor_table
+        FactorTable.new(no_work: 4.0, below_30: 6.0, below_50: 7.0, below_75: 8.0, above_75: 8.0)
+      end
+
+      class FactorTable
+        attr_reader :no_work, :below_30, :below_50, :below_75, :above_75
+
+        def initialize(factors)
+          @no_work = factors.fetch(:no_work)
+          @below_30 = factors.fetch(:below_30)
+          @below_50 = factors.fetch(:below_50)
+          @below_75 = factors.fetch(:below_75)
+          @above_75 = factors.fetch(:above_75)
+        end
+      end
+
       class EnergyWorkloadRatio
-        def initialize(workload_energy, maintenance_energy)
+        def initialize(workload_energy, maintenance_energy, factor_table)
           @workload_energy = workload_energy
           @maintenance_energy = maintenance_energy
+          @factor_table = factor_table
         end
 
         def factor
           if workload_energy_quota == 0
-            4.0
+            factor_table.no_work
           elsif workload_energy_quota < 0.30
-            6.0
+            factor_table.below_30
           elsif workload_energy_quota < 0.50
-            7.0
+            factor_table.below_50
           elsif workload_energy_quota < 0.75
-            8.0
+            factor_table.below_75
           else
-            8.0
+            factor_table.above_75
           end
         end
 
         private
 
-        attr_reader :workload_energy, :maintenance_energy
+        attr_reader :workload_energy, :maintenance_energy, :factor_table
 
         def workload_energy_quota
           workload_energy.to_f / maintenance_energy.to_f
