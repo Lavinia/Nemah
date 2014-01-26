72004e1aa2e5e2f91910dff64128d7f6ed5da1c3
Lennart and Kim <lennart@devl.se>
2014-01-26 11:07:42 +0100

    Add enough protein and solids.

--- lib/nemah/ration.rb
+++ lib/nemah/ration.rb
@@ -9,9 +9,23 @@ module Nemah
 
     def enough_energy?
       given_energy = fodder_list.each.inject(0) do |total_energy, (fodder, amount)|
-        total_energy += amount * fodder.energy.value
+        total_energy += amount * fodder.energy.value(need.energy.unit)
       end
       need.energy.min <= given_energy
+    end
+
+    def enough_protein?
+      given_protein = fodder_list.each.inject(0) do |total_protein, (fodder, amount)|
+        total_protein += amount * fodder.protein.value(need.protein.unit)
+      end
+      need.protein.min <= given_protein
+    end
+
+    def enough_solids?
+      given_solids = fodder_list.each.inject(0) do |total_solids, (fodder, amount)|
+        total_solids += amount * fodder.solids.value(need.solids.unit)
+      end
+      need.solids.min <= given_solids
     end
   end
 end
--- spec/nemah/ration_spec.rb
+++ spec/nemah/ration_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Nemah::Ration do
   describe '#enough_energy?' do
     it "returns true when the horse's energy need is met" do
-      energy = double('energy', min: 65)
+      energy = double('energy', min: 65, unit: :MJ)
       need = double('need', energy: energy)
       fodders = { Nemah::Fodder.new('hay', { energy: 6.5 }) => 10 }
       fodder_list = Nemah::FodderList.new(fodders)
@@ -13,13 +13,57 @@ describe Nemah::Ration do
     end
 
     it 'returns false when the energy need is not met' do
-      energy = double('energy', min: 65.1)
+      energy = double('energy', min: 65.1, unit: :MJ)
       need = double('need', energy: energy)
       fodders = { Nemah::Fodder.new('hay', { energy: 6.5 }) => 10 }
       fodder_list = Nemah::FodderList.new(fodders)
       ration = Nemah::Ration.new(need, fodder_list)
 
       expect(ration.enough_energy?).to be_false
+    end
+  end
+
+  describe '#enough_protein?' do
+    it "returns true when the horse's protein need is met" do
+      protein = double('protein', min: 330, unit: :g)
+      need = double('need', protein: protein)
+      fodders = { Nemah::Fodder.new('hay', { protein: 33 }) => 10 }
+      fodder_list = Nemah::FodderList.new(fodders)
+      ration = Nemah::Ration.new(need, fodder_list)
+
+      expect(ration.enough_protein?).to be_true
+    end
+
+    it "returns false when the horse's protein need is not met" do
+      protein = double('protein', min: 331, unit: :g)
+      need = double('need', protein: protein)
+      fodders = { Nemah::Fodder.new('hay', { protein: 33 }) => 10 }
+      fodder_list = Nemah::FodderList.new(fodders)
+      ration = Nemah::Ration.new(need, fodder_list)
+
+      expect(ration.enough_protein?).to be_false
+    end
+  end
+
+  describe '#enough_solids?' do
+    it "returns true when the horse's solids need is met" do
+      solids = double('solids', min: 7, unit: :kg)
+      need = double('need', solids: solids)
+      fodders = { Nemah::Fodder.new('hay', { solids: 70 }) => 10 }
+      fodder_list = Nemah::FodderList.new(fodders)
+      ration = Nemah::Ration.new(need, fodder_list)
+
+      expect(ration.enough_solids?).to be_true
+    end
+
+    it "returns false when the horse's solids need is not met" do
+      solids = double('solids', min: 7.1, unit: :kg)
+      need = double('need', solids: solids)
+      fodders = { Nemah::Fodder.new('hay', { solids: 70 }) => 10 }
+      fodder_list = Nemah::FodderList.new(fodders)
+      ration = Nemah::Ration.new(need, fodder_list)
+
+      expect(ration.enough_solids?).to be_false
     end
   end
 end
